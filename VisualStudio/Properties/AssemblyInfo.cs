47128e5070735f0d76a1cf93e93ec1a424e19518
Waltz <onelolzerg@gmail.com>
2025-06-25 21:09:18 +0300

    up

--- VisualStudio/Properties/AssemblyInfo.cs
+++ VisualStudio/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 [assembly: AssemblyTitle("FoodsByCalorieWaltz")]
 [assembly: AssemblyCopyright("Created by Waltz")]
 
-[assembly: AssemblyVersion("1.0.10")]
-[assembly: AssemblyFileVersion("1.0.10")]
-[assembly: MelonInfo(typeof(CalorieWaltz.CWMain), "FoodsByCalorieWaltz", "1.0.10", "Waltz")]
+[assembly: AssemblyVersion("1.0.11")]
+[assembly: AssemblyFileVersion("1.0.11")]
+[assembly: MelonInfo(typeof(CalorieWaltz.CWMain), "FoodsByCalorieWaltz", "1.0.11", "Waltz")]
 
 [assembly: MelonGame("Hinterland", "TheLongDark")]
